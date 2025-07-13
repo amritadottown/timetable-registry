204c07a6bdbe3289ce1e316dc946f3780c8ab238
Adithya Nair <adithyanair121@gmail.com>
2025-07-13 17:20:12 +0530

    Added an index-generator script.

---
+++ registry/index-gen.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = "files";
+const years = fs.readdirSync(rootDir);
+
+const outputData = {};
+outputData.version = 1;
+outputData.timetables = {};
+
+for (const year of years) {
+  const yearPath = path.join(rootDir, year);
+  const branches = fs.readdirSync(yearPath);
+  outputData.timetables[year] = {};
+  for (const branch of branches) {
+    const branchPath = path.join(yearPath, branch);
+    const sems = fs.readdirSync(branchPath).map((str) => str[0]);
+    outputData.timetables[year][branch] = sems;
+  }
+}
+
+fs.writeFileSync("index.json", JSON.stringify(outputData, null, 4), "utf-8");
